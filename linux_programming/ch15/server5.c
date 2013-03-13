df4d9c1c80ee0b982ca73c6082034f58685eac05
fancylinq <fancylinq@163.com>
2013-03-13 20:10:23 +0800

    complete

--- linux_programming/ch15/server5.c
+++ linux_programming/ch15/server5.c
@@ -2,9 +2,8 @@
 #include <sys/socket.h>
 #include <stdio.h>
 #include <netinet/in.h>
-#include <signal.h>
-#include <arpa/inet.h>
-#include <sys/un.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
 #include <stdlib.h>
 
@@ -14,6 +13,8 @@ int main(int argc, const char *argv[])
   int server_len, client_len;
   struct sockaddr_in server_address;
   struct sockaddr_in client_address;
+  int result;
+  fd_set readfds, testfds;
 
   server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
   server_address.sin_family = AF_INET;
@@ -23,23 +24,47 @@ int main(int argc, const char *argv[])
   bind(server_sockfd, (struct sockaddr *)&server_address, server_len);
 
   listen(server_sockfd, 5);
-  signal(SIGCHLD, SIG_IGN);
+
+  FD_ZERO(&readfds);
+  FD_SET(server_sockfd, &readfds);
 
   while (1) {
     char ch;
-    printf("server waiting\n");
-    client_len = sizeof(client_address);
-    client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_address, &client_len);
+    int fd;
+    int nread;
 
-    if (fork() == 0) {
-      read(client_sockfd, &ch, 1);
-      ch++;
-      write(client_sockfd, &ch, 1);
-      close(client_sockfd);
-      exit(0);
-    } else {
-      close(client_sockfd);
+    testfds = readfds;
+
+    printf("server waiting\n");
+    result = select(FD_SETSIZE, &testfds, (fd_set *)0, (fd_set *)0, (struct timeval *)0);
+
+    if (result < 1) {
+      perror("server5");
+      exit(1);
+    }
+
+    for (fd = 0; fd < FD_SETSIZE; fd++) {
+      if (FD_ISSET(fd, &testfds)) {
+        if (fd == server_sockfd) {
+          client_len = sizeof(client_address);
+          client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_address, &client_len);
+          FD_SET(client_sockfd, &readfds);
+          printf("adding client on fd %d\n", client_sockfd);
+        } else {
+          ioctl(fd, FIONREAD, &nread);
+          if (nread == 0) {
+            close(fd);
+            FD_CLR(fd, &readfds);
+            printf("removing client on fd %d\n", fd);
+          } else {
+            read(fd, &ch, 1);
+            sleep(5);
+            printf("serving client on fd %d\n", fd);
+            ch++;
+            write(fd, &ch, 1);
+          }
+        }
+      } 
     }
   }
-  return 0;
 }
